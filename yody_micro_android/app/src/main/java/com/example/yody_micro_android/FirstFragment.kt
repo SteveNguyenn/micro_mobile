6efdee299790b4c8813274db3a30d6f6c11a6020
Steve <chaunguyen.mobiledeveloper@gmail.com>
2024-03-13 11:35:42 +0700

    update android

---
+++ yody_micro_android/app/src/main/java/com/example/yody_micro_android/FirstFragment.kt
@@ -0,0 +1,50 @@
+package com.example.yody_micro_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.yody_micro_android.databinding.FragmentFirstBinding
+import io.flutter.embedding.android.FlutterActivity;
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+private var _binding: FragmentFirstBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+      _binding = FragmentFirstBinding.inflate(inflater, container, false)
+      return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            val engine = FlutterActivity.withNewEngine()
+            val bundle = Bundle()
+            bundle.putString("io.flutter.EntrypointUri", "package:yody_profile/main.dart")
+            startActivity(
+                engine.build(view.context),
+                bundle
+            )
+        }
+    }
+
+override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
